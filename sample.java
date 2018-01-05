8670c11e6d64b97c593ddcbe4f23a0e9f9e02ee0
gsbinu <binu@growingstars.com>
2018-01-05 14:57:39 +0530

    Test
    Test

---
+++ sample.java
@@ -0,0 +1,15 @@
+
+public class sample {
+
+	public static void main(String arg[]){
+		try{
+			System.out.println("Enter the value of x ");
+		int x= System.in.read();		
+		System.out.println("Enter the value of y ");
+		int y= System.in.read();
+		System.out.println("The result of adding "+x+" and "+y+" is "+(x+y));
+		}catch(Exception e){
+			
+		}
+	}
+}
